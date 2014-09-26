0e4aa701a4b5dcd829b65f747ad270216ffcc98f
Khaled <sghaierkhaled@live.com>
2014-09-26 21:47:12 +0200

    new USerVo class

---
+++ src/main/java/com/webcaisse/ws/model/UserVo.java
@@ -0,0 +1,46 @@
+package com.webcaisse.ws.model;
+
+public class UserVo {
+
+	private String nom;
+	
+	private String prenom;
+	
+	private String adresse;
+	
+	private String nomSociete;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getNomSociete() {
+		return nomSociete;
+	}
+
+	public void setNomSociete(String nomSociete) {
+		this.nomSociete = nomSociete;
+	}
+	
+	
+}
