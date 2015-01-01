c3a73c07d64377927c960d409b1e6083398f9dab
Khaled <sghaierkhaled@live.com>
2015-01-01 23:36:17 +0100

    ajout d'autre champs au formulaire

--- src/main/java/com/webcaisse/ws/model/ClientIn.java
+++ src/main/java/com/webcaisse/ws/model/ClientIn.java
@@ -1,12 +1,89 @@
 package com.webcaisse.ws.model;
 
-public class ClientIn {
+import java.util.Date;
 
-	private String prenom ;
+public class ClientIn {
+	
+	
 	private String telephone ;
 	private String email ;
 	private Long id  ;
 	private  String nom ;
+    private String code1;
+	private String code2;
+	private String code3;
+	private String codePostale;
+	private Integer etage ;
+	private String immeuble;
+	private String interphone ;
+	private String numeroRue;
+	private String nomRue ;
+	private String ville ;
+	private String prenom ;
+	
+	
+	
+	public String getNomRue() {
+		return nomRue;
+	}
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+	public String getCode1() {
+		return code1;
+	}
+	public void setCode1(String code1) {
+		this.code1 = code1;
+	}
+	public String getCode2() {
+		return code2;
+	}
+	public void setCode2(String code2) {
+		this.code2 = code2;
+	}
+	public String getCode3() {
+		return code3;
+	}
+	public void setCode3(String code3) {
+		this.code3 = code3;
+	}
+	public String getCodePostale() {
+		return codePostale;
+	}
+	public void setCodePostale(String codePostale) {
+		this.codePostale = codePostale;
+	}
+	public Integer getEtage() {
+		return etage;
+	}
+	public void setEtage(Integer etage) {
+		this.etage = etage;
+	}
+	public String getImmeuble() {
+		return immeuble;
+	}
+	public void setImmeuble(String immeuble) {
+		this.immeuble = immeuble;
+	}
+	public String getInterphone() {
+		return interphone;
+	}
+	public void setInterphone(String interphone) {
+		this.interphone = interphone;
+	}
+	public String getNumeroRue() {
+		return numeroRue;
+	}
+	public void setNumeroRue(String numeroRue) {
+		this.numeroRue = numeroRue;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
 	private Long idSociete ;
 	
 	public Long getIdSociete() {
--- src/main/java/com/webcaisse/ws/model/ClientOut.java
+++ src/main/java/com/webcaisse/ws/model/ClientOut.java
@@ -2,14 +2,85 @@ package com.webcaisse.ws.model;
 
 public class ClientOut {
 	
-	private  String nom ;
-	private String prenom ;
 	private String telephone ;
 	private String email ;
+	private String prenom ;
 	private Long id  ;
+	private  String nom ;
+    private String code1;
+	private String code2;
+	private String code3;
+	private String codePostale;
+	private Integer etage ;
+	private String immeuble;
+	private String interphone ;
+	private String numeroRue;
+	private String nomRue ;
+	private String ville ;
+	
 	
 
 
+	public String getNomRue() {
+		return nomRue;
+	}
+	public void setNomRue(String nomRue) {
+		this.nomRue = nomRue;
+	}
+	public String getCode1() {
+		return code1;
+	}
+	public void setCode1(String code1) {
+		this.code1 = code1;
+	}
+	public String getCode2() {
+		return code2;
+	}
+	public void setCode2(String code2) {
+		this.code2 = code2;
+	}
+	public String getCode3() {
+		return code3;
+	}
+	public void setCode3(String code3) {
+		this.code3 = code3;
+	}
+	public String getCodePostale() {
+		return codePostale;
+	}
+	public void setCodePostale(String codePostale) {
+		this.codePostale = codePostale;
+	}
+	public Integer getEtage() {
+		return etage;
+	}
+	public void setEtage(Integer etage) {
+		this.etage = etage;
+	}
+	public String getImmeuble() {
+		return immeuble;
+	}
+	public void setImmeuble(String immeuble) {
+		this.immeuble = immeuble;
+	}
+	public String getInterphone() {
+		return interphone;
+	}
+	public void setInterphone(String interphone) {
+		this.interphone = interphone;
+	}
+	public String getNumeroRue() {
+		return numeroRue;
+	}
+	public void setNumeroRue(String numeroRue) {
+		this.numeroRue = numeroRue;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
 	public Long getId() {
 		return id;
 	}
